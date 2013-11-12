f6c0c0981c2ffdb4033f13ad3eb4630c15ffa12d
amarathimi <amarathimi@gmail.com>
2013-11-12 21:36:03 +0200

    Merged MIQUEL folder to main project.
    Merged MIQUEL folder into the main project and tried to separate the
    javascript code and processing code a little bit.

---
+++ README.txt
@@ -0,0 +1,13 @@
+Run project from VisualMixer.html
+
+VisualMixer.pde - Processing code
+visualmixer.js - javascript code
+
+css/menu.css - css for the menu
+css/visualmixer - css for the mixer
+
+libraries used:
+processing-1.4.1.min.js
+hammer.min.js
+jquery.hammer.min.js
+jquery-2.0.3.min.js
---
+++ visualmixer.js
@@ -0,0 +1,49 @@
+
+(function(visualmixer) {
+  // The global jQuery object is passed as a parameter
+  visualmixer(window.jQuery, window, document);
+}
+(function($, window, document) {
+  // The $ is now locally scoped 
+
+  // Listen for the jQuery ready event on the document
+  $(function() {
+    // The DOM is ready!
+    console.log('The DOM is ready!');
+
+    //
+    // HAMMER.JS
+    //
+	console.log('Init Hammer.js!');
+    var hammer = Hammer(document.getElementById("visual-mixer-canvas"));
+
+    hammer.on("hold touch dragstart drag dragend", function(event) {
+	  var PInstance = Processing.getInstanceById("visual-mixer-canvas");
+      event.gesture.preventDefault();
+      var x = event.gesture.touches[0].pageX; 
+      var y = event.gesture.touches[0].pageY;
+
+      switch (event.type) {
+      case "hold":
+        PInstance.handleHold(x,y);
+        break;
+      case "touch":
+        PInstance.handleTouch(x,y);
+        break;
+      case "dragstart":
+        PInstance.startDrag();
+        break;
+      case "drag":
+        PInstance.processDrag(x, y);
+        break;
+      case "dragend":
+        PInstance.endDrag(x, y);
+        break;
+      }
+    }
+    );
+  }
+  );
+}
+));
+
